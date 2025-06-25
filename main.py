fbc8e643f27728073121d06c06c89ea07cb11810
Sahiti Amirapu <28sahiti@gmail.com>
2025-06-25 18:10:06 +0530

    Day 3: Added keyword + date filters to FastAPI

--- main.py
+++ main.py
@@ -68,6 +68,42 @@ account = SavingsAccount("Sahiti", 1000)
 class Transaction(BaseModel):
     amount: float
 
+
+def search_transactions(owner, keyword=None, date_from=None, date_to=None):
+        results=[]
+        file_path = f"{owner}_transactions.csv"
+
+        if not os.path.exists(file_path):
+            raise HTTPException(status_code=404, detail="Transaction file not found.")
+        
+        from datetime import datetime
+        
+        try:
+            if date_from:
+                date_from = datetime.strptime(date_from, "%Y-%m-%d")
+            if date_to:
+                date_to=datetime.strptime(date_to, "%Y-%m-%d")
+        except ValueError:
+            raise HTTPException(status_code=400, detail="Invalid data format. Use YYYY-MM-DD.")
+        print("parsing date_from:", date_from)
+        
+        with open(file_path,mode='r')as f:
+            reader = csv.DictReader(f)
+            for row in reader:
+                row_date = datetime.strptime(row['Date'],"%Y-%m-%d %H:%M:%S" )
+
+                if keyword and keyword.lower() not in row["Action"].lower():
+                    continue
+                if date_from and row_date< date_from:
+                    continue
+                if date_to and row_date< date_to:
+                    continue
+
+                
+                results.append(row)
+        
+        return results
+
 # ------------ API Routes ------------
 
 @app.get("/balance")
@@ -101,4 +137,13 @@ def get_transactions():
             return list(reader)
     except FileNotFoundError:
         raise HTTPException(status_code=404, detail="Transaction file not found.")
+    
+@app.get("/search")
+def search(keyword: str=None, date_from: str=None, date_to:str=None):
+    return search_transactions("Sahiti",keyword,date_from,date_to)
+
+@app.get("/debug-all")
+def debug_all():
+    with open(account.log_file, mode='r') as f:
+        return list(csv.DictReader(f))
 
